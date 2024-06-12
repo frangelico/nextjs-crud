e9e50678834a31d84e90d0bc52304d669fd3dbdd
Stephen Jang <stephenjang@coderedfour.com>
2024-06-13 00:37:29 +0800

    Pages wallets and users

---
+++ src/app/ui/dashboard/nav-links.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+// Map of links to display in the side navigation.
+// Depending on the size of the application, this would be stored in a database.
+const links = [
+  { name: 'Home', href: '/', icon: HomeIcon },
+  {
+    name: 'Wallets',
+    href: '/wallets',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Users', href: '/users', icon: UserGroupIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-900 p-3 text-sm font-medium hover:bg-gray-400 hover:text-black-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-gray-400 text-black-600': pathname === link.href,
+              }
+            )}
+            
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
